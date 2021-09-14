f68c75df5a8e7689ff76696e26fb431acac06bd1
Jcrystal <gomdorij@naver.com>
2021-09-14 22:28:12 +0900

    0914

---
+++ Jcry_Frontend_class/210911/main.js
@@ -0,0 +1,7 @@
+// // html 2
+// var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//   return new bootstrap.Tooltip(tooltipTriggerEl)
+// })
+
+// html 3
