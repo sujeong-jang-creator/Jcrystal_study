b3b45ce8f251623556b2c5c5e48efd1daff829ee
JANGSUJEONG <gomdorij@naver.com>
2022-08-04 14:10:28 +0900

    heroService 추가

--- Jcry_Angular/my-app/src/app/heroes/heroes.component.ts
+++ Jcry_Angular/my-app/src/app/heroes/heroes.component.ts
@@ -3,6 +3,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 // 컴포넌트 클래스 지정. @Component
 // 컴포넌트 : 템플릿이 존재하는 디렉티브.
 // 디렉티브 : Augular 애플리케이션 안에 있는 엘리먼트에 어떤 동작을 추가하는 클래스
@@ -17,18 +18,23 @@ import { HEROES } from '../mock-heroes';
 // 컴포넌트는 export(추출)해야 다른 모듈에서 import 가능하다.
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
   selectedHero?: Hero;
+  heroes: Hero[] = [];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
+  
   ngOnInit() {
+    this.getHeroes();
   }
-
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-
+  
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
   // ngOnInit : 라이프싸이클 후킹함수 > angular의 라이프싸이클에 간섭하는(영향을 미치는) 함수.
   // hooking : OS나 응용 소프트웨어 등 각종 컴퓨터 프로그램에서 소프트웨어 구성 요소간에 발생하는 함수호출, 메세지, 이벤트 등을 중간에서 바꾸거나 가로채는 명령, 방법, 기술, 행위.
   //          이때 이렇게 간섭된 함수호출, 이벤트, 메세지 등을 처리하는 코드를 훅이라고 한다. 빼돌리거나 간섭하는 코드.
